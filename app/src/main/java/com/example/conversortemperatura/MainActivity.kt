2d9608f4369f4d79cf3d4b3fbdd77c47c12caa72
Bruno Otávio Souza de Ávila <92824908+BrunoSA89@users.noreply.github.com>
2022-12-09 11:27:37 -0300

    nova modificaçao

--- app/src/main/java/com/example/conversortemperatura/MainActivity.kt
+++ app/src/main/java/com/example/conversortemperatura/MainActivity.kt
@@ -34,7 +34,36 @@ class MainActivity : AppCompatActivity() {
     fun converterTemp(){
     val  temperatura = binding.textEntrada.text.toString().toDouble()
         //variavel / captura informacao do ID / pega string e converte double que é casa decimal
+    val gFahrenheit = (temperatura * 1.8) + 32
+    val gKelvin  = temperatura + 273
+    val cC = temperatura
+    val fahrenheitG = (temperatura - 32) / 1.8
+    val kelvinG = (temperatura + 459.67) * 5 / 9
+    val fF = temperatura
+    val kelvinC = temperatura - 273
+    val kelvinF = ((temperatura - 273) * 9 / 5 ) + 32
+    val kK = temperatura
 
+        when{
+            binding.butonC1.isChecked  && binding.buttonF2.isChecked ->
+                binding.textResultado.setText(".2f".format(gFahrenheit))
+            binding.butonC1.isChecked && binding.buttonK2.isChecked ->
+                binding.textResultado.setText(".%2".format(gKelvin))
+            binding.butonC1.isChecked  && binding.buttonC2.isChecked ->
+                binding.textResultado.setText(".2f".format(cC))
+            binding.buttonF1.isChecked && binding.buttonC2.isChecked ->
+                binding.textResultado.setText(".%2".format(fahrenheitG))
+            binding.buttonF1.isChecked && binding.buttonK2.isChecked ->
+                binding.textResultado.setText(".%2".format(kelvinG))
+            binding.buttonF1.isChecked && binding.buttonF2.isChecked ->
+                binding.textResultado.setText(".%2".format(fF))
+            binding.buttonK1.isChecked && binding.buttonC2.isChecked ->
+                binding.textResultado.setText(".%2".format(kelvinC))
+            binding.buttonK1.isChecked && binding.buttonF2.isChecked ->
+                binding.textResultado.setText(".%2".format(kelvinF))
+            binding.buttonK1.isChecked && binding.buttonK2.isChecked ->
+                binding.textResultado.setText(".%2".format(kK))
+        }
     }
 }
 
